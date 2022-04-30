c51eccddb41d9dcd932e2f3129c60cea1ee06451
nhidalgo20 <nhidalgo@csus.edu>
2022-04-30 13:47:41 -0700

    API works

--- src/main/java/com/cardealer/Controller/CustomerController.java
+++ src/main/java/com/cardealer/Controller/CustomerController.java
@@ -6,6 +6,7 @@ import com.cardealer.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 @RestController
